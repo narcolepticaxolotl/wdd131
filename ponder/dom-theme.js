27beeeaa982a85b712ca46c55464489cc551a3f8
narcolepticaxolotl <elijahjrlarson@gmail.com>
2026-01-17 20:50:15 -0700

    Completed W02 Ponder, "Computational Thinking."

---
+++ ponder/dom-theme.js
@@ -0,0 +1,24 @@
+// https://wddbyui.github.io/wdd131/ponder/ponder_wk2b.html
+let selectElem = document.querySelector('#theme-select');
+let pageContent = document.querySelector('body');
+
+selectElem.addEventListener('change', changeTheme);
+
+function changeTheme() {
+    let current = selectElem.value;
+    if (current === 'ocean') {
+        document.body.style.backgroundImage = "url('https://wddbyui.github.io/wdd131/images/ocean.jpg')";
+        pageContent.style.fontFamily = "Papyrus, fantasy";
+        console.log(pageContent.style.fontFamily)
+    } else if (current === 'forest') {
+        document.body.style.backgroundImage = "url('https://wddbyui.github.io/wdd131/images/forest.jpg')";
+        pageContent.style.fontFamily = "Impact, sans-serif";
+    } else if (current === 'desert') {
+        document.body.style.backgroundImage = "url('https://wddbyui.github.io/wdd131/images/desert.jpg')";
+        pageContent.style.fontFamily = "'Big Caslon', serif";
+    } else {
+        // default appeareance
+        document.body.style.backgroundImage = "none";
+        pageContent.style.fontFamily = "Georgia, serif";
+    }
+}
